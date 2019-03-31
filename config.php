697ffcee2c085880be53cdecab3b9e320c6e78c2
wfntwl <49130340+wfntwl@users.noreply.github.com>
2019-04-01 04:58:36 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyDb7sLL8QNW1DBt8YVLvMBOAjfNtY-7O84');
+define('GJ_CODE', 'HK');
+define('SITE_NAME', '熊猫乐园');
+define('TITLENAME', 'Panda Youtube');
+define('EN2DEKEY', '_hTnQP/:xMgo2;)QAq&3O>^);@"xJE');
+define('EMAIL', '79117112@qq.com');
+?>
