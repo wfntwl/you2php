38be1f2476eec82fef66423fa307de09f4d7cf61
wfntwl <49130340+wfntwl@users.noreply.github.com>
2019-04-01 04:59:10 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyDb7sLL8QNW1DBt8YVLvMBOAjfNtY-7O84');
+define('APIKEY', 'AIzaSyDI-5DFPTo0iUARMyjbwkqiiUBbWfjy0W8');
 define('GJ_CODE', 'HK');
-define('SITE_NAME', '熊猫乐园');
+define('SITE_NAME', 'Panda Youtube');
 define('TITLENAME', 'Panda Youtube');
 define('EN2DEKEY', '_hTnQP/:xMgo2;)QAq&3O>^);@"xJE');
 define('EMAIL', '79117112@qq.com');
